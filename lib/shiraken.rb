395b00529e621bca101d201edc88eea8aec2995f
shirakurak <surakura@hotmail.co.jp>
2022-09-18 15:30:44 +0900

    make skelton

---
+++ lib/shiraken.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "shiraken/version"
+
+module Shiraken
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/shiraken_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Shiraken do
+  it "has a version number" do
+    expect(Shiraken::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
