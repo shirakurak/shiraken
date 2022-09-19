fd879e28c5271b357a9bdb6ff7f1e8d19ebc47a9
shirakurak <surakura@hotmail.co.jp>
2022-09-19 11:58:56 +0900

    add howise cmd

--- lib/shiraken/cli.rb
+++ lib/shiraken/cli.rb
@@ -12,5 +12,20 @@ module Shiraken
         def github
             puts "https://github.com/shirakurak"
         end
+
+        desc "howise", "let the brain solve the problem"
+        def howise
+            puts "今考えていることは？"
+            input1 = STDIN.gets.chomp
+            puts "----------"
+            puts "どうすれば" + input1 + "〇〇か？"
+            input2 = STDIN.gets.chomp
+            puts "----------"
+            puts "「どうすれば" + input1 + input2 + "か？」"
+            puts "↑"
+            task1 = STDIN.gets.chomp
+            puts "----------"
+            puts "どうすれば「" + task1 + "」を進められるか？"
+        end
     end
 end
